44b264161b5c91ac83e4707c640bf1290ef09f34
Pedro Perrone <psperrone@hotmail.com>
2019-04-27 15:42:31 -0300

    Send and receive file

--- include/connection.h
+++ include/connection.h
@@ -1,16 +1,33 @@
 #ifndef __connection__
 #define __connection__
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <pthread.h>
+#include <math.h>
+
+#define PACKAGE_SIZE 4096
+
+typedef struct package {
+    int totalSize;
+    int index;
+    int dataSize;
+    char data[PACKAGE_SIZE];
+} PACKAGE;
 
 void* processConnection(void* clientSocket);
 void initializeMainSocket(int *serverfd, struct sockaddr_in *address);
 void handleNewRequest(int mainSocket, struct sockaddr_in address);
+int sendFile(FILE *fileDescriptor, int socketDescriptor);
+int receiveFile(int socketDescriptor);
+int receivePackage(PACKAGE *package, int socketDescriptor);
+int writePackage(PACKAGE package, FILE *file);
+int calculateFileSize(FILE *fileDescriptor);
 
 #endif
